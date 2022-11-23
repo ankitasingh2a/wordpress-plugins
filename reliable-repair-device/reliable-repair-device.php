371b7521b2fc743530124cfc0cc7b00246b00b38
Ankita Singh <ankitasingh2a@gmail.com>
2022-11-23 14:18:08 +0530

    Update reliable-repair-device.php

--- reliable-repair-device/reliable-repair-device.php
+++ reliable-repair-device/reliable-repair-device.php
@@ -17,7 +17,7 @@
  * Plugin URI:        http://example.com/reliable-repair-device-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Ankita
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
